30cee2a9e60179f2c04562fed11c965f87acd51c
Niklas Furberg <niklas.furberg@gmail.com>
2026-03-26 21:19:01 +0100

    search component bare bones spa setup

---
+++ search-component/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>Search Component</h1>
+      <p>This is a bare-bones React + Vite project.</p>
+    </div>
+  );
+}
