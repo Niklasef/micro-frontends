d4d54b332ea2f3757a0331f4bc062e09c68f0dbd
Niklas Furberg <niklas.furberg@gmail.com>
2026-03-26 21:38:04 +0100

    made search component into web component

--- search-component/src/main.tsx
+++ search-component/src/main.tsx
@@ -1,9 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot, type Root } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/**
+ * Web-component wrapper that mounts the React app into its own shadow DOM.
+ */
+class SearchComponentElement extends HTMLElement {
+  private _root?: Root;
+
+  connectedCallback() {
+    if (this._root) return; // already mounted
+
+    const mountPoint = this.attachShadow({ mode: "open" });
+    this._root = createRoot(mountPoint);
+    this._root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }
+
+  disconnectedCallback() {
+    this._root?.unmount();
+    this._root = undefined;
+  }
+}
+
+if (!customElements.get("search-component")) {
+  customElements.define("search-component", SearchComponentElement);
+}
