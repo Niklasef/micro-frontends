810d057c99826be3b1a09ca5afd1db476a1300d0
Niklas Furberg <niklas.furberg@gmail.com>
2026-05-10 19:11:14 +0200

    improved logging, use module NodeNext

--- search-api/src/auth.ts
+++ search-api/src/auth.ts
@@ -1,7 +1,13 @@
+import "./env.js";
 import { jwtVerify, createRemoteJWKSet, type JWTPayload } from "jose";
 import { URL } from "node:url";
 
-const issuer = process.env.KEYCLOAK_ISSUER!;
+const issuer = process.env.KEYCLOAK_ISSUER;
+if (!issuer) {
+  throw new Error(
+    "Missing KEYCLOAK_ISSUER environment variable (e.g., http://localhost:8080/realms/furmountain)"
+  );
+}
 const JWKS = createRemoteJWKSet(new URL(`${issuer}/protocol/openid-connect/certs`));
 
 export async function verifyToken<T extends JWTPayload = JWTPayload>(token: string): Promise<T> {
