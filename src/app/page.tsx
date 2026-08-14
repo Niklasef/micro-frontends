d4e32289d66aa313cf2f5363eabe61ecf3032fdb
Niklas Furberg <niklas.furberg@gmail.com>
2026-08-14 23:54:41 +0200

    minimal main site, and working docker fil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 import HomeClient from "./HomeClient";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth-options";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
 
   return (
     <HomeClient
-      userName={session?.user?.name ?? null}
-      userEmail={session?.user?.email ?? null}
-      isAuthenticated={!!session}
-      session={session!}
     />
   );
 }
